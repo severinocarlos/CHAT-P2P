f2da5dabac29eaae9353d76b15c292483edcb6e9
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-04-19 14:38:39 -0300

    ajustes

--- client_1.py
+++ client_1.py
@@ -2,11 +2,11 @@ import socket
 import time
 import threading
 import random
-
+import datetime
 
 class Client:
     """        
-        -Criação de um web socket baseado em protocolo TCP-
+        - Criação de um web socket baseado em protocolo TCP -
         buildSocket: Criação/conexão com o usuário/servidor
         main: administra e cria as threads
         seqNum: retorna um inteiro para sair formatado em sequência na mensagem
@@ -19,36 +19,31 @@ class Client:
         self.users = ['Severino', 'Lucas', 'Joao', 'Victor', 'Neto', 'Alejandro']
         self.HOST = 'localhost'
         self.PORT_SERVER = 55555
-        self.flag = True
 
     def buildSocket(self, port):
         self.CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if self.flag:
-            self.CLIENT.connect((self.HOST, port))
-        else:
-            self.CLIENT.connect((self.HOST, port))
-        
+        self.CLIENT.connect((self.HOST, port))
+ 
         return
     
     def main(self):
 
-        Client.buildSocket(self, 55555)
+        Client.buildSocket(self, 55555) # criando o socket e conectando com o servidor
         self.USER = random.choice(self.users)
         
-        print(f'Olá {self.USER}, esperando conexão com seu amigo...')
-        addr = self.CLIENT.recvfrom(1024)
+        print(f'Olá {self.USER}, aguardando confirmação...')
+        addr = self.CLIENT.recvfrom(1024) # recebimento do endereço do outro user
         
         # obtendo o endereço IP e a porta do outro user
         addrUser = int(addr[0].decode())
-        # address = (self.HOST, addrCloe)
+        print(f'Endereço de porta do outro usuário: {addrUser}')
 
         # fechadno a conexão com o servidor para deixar ele pronto para uma próxima conexão
         self.CLIENT.close()
         #criando outro socket para fazer a conexão com o outro user
-        self.flag = False
         Client.buildSocket(self, addrUser)
 
-
+        #inicialização das threads
         th1 = threading.Thread(target=Client.send, args= [self])
         th2 = threading.Thread(target=Client.receive, args=[self])
 
@@ -61,15 +56,15 @@ class Client:
 
     def send(self):
         while True:
-            print('> Se você deseja encerrar a conexão digite [encerrar]')
-            msg = input('> ')
+            print('\n> Se você deseja encerrar a conexão digite [encerrar]')
+            msg = input('')
 
             try:
                 if msg == 'encerrar':
                     self.CLIENT.sendall(bytes(f'A conexão foi encerrado por {self.USER}','utf-8'))
                     self.CLIENT.close()
                 else:
-                    self.CLIENT.sendall(bytes(f'{Client.seqNum(self)}. [{time.strftime("%X")}] <{self.USER}> {msg}', 'utf-8'))
+                    self.CLIENT.sendall(bytes(f'{Client.seqNum(self)}. [{time.strftime("%X")} {datetime.date.today()}]  <{self.USER}> {msg}', 'utf-8'))
                     print('*** Mensagem enviada com sucesso! ***')
             except:
                 self.CLIENT.close()

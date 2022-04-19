dd52787511a4b43d6aec73088f9a8c7cfd71f985
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-04-19 14:39:10 -0300

    add documentação

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 import socket
-import time
-
 
 HOST = 'localhost'
-
 
 first = True
 
@@ -12,20 +9,28 @@ while True:
     SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.bind((HOST, 55555))
     SERVER.listen(2) # obs: vai ser conectados dois clients a esse server
-    if first > 0:
-        print('Esperando uma próxima conexão')
     
+    if not first:
+        print('Esperando uma próxima conexão')
+    #aceitando a conexão dos dois clients
     CLIENT_1, address = SERVER.accept()
     CLIENT_2, address_2 = SERVER.accept()
+
+    print('-'*32)
     print('Conexão estabelecida!')
-    print(f'1º Usuário: {address}')
-    print(f'2º Usuário: {address_2}')
+    print(f'\n1º Usuário com endereço IP: {address[0]} e porta {address[1]}')
+    print(f'2º Usuário com endereço IP: {address_2[0]} e porta {address_2[1]}')
     first = False
+    
     while not sendIP:
-        print('lalala')
+        '''
+            Só irá rodar até trocar as informações de endereços/porta dos dois usuários
+            e posteriomente vai fechar e conexão com esses clients ficando já pronto para uma próxima conexão!
+        '''
         CLIENT_1.sendto(bytes(f'{address_2[1]}', 'utf-8'), address_2)
         CLIENT_2.sendto(bytes(f'{address[1]}', 'utf-8'), address)
-        print('Envio concluído')
+        print('Envio concluído\n')
+        print('-'*32)
         sendIP = True
         CLIENT_1.close()
         CLIENT_2.close()
